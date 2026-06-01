1357804b9e348350e887373d3ddbde47a5ad4c14
Santiago <gutierrezamorochokevinsantiago@gmail.com>
2026-06-01 14:53:43 -0500

    feat(api/vendelo): add admin shipping DTOs, ShippingAdminService, and 6 logistics endpoints

--- apps/api/src/vendelo/vendelo.module.ts
+++ apps/api/src/vendelo/vendelo.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common'
 import { InfrastructureModule } from '../infrastructure/infrastructure.module'
 import { VendeloController } from './vendelo.controller'
 import { VendeloWebhookController } from './vendelo-webhook.controller'
+import { ShippingAdminService } from './services/shipping-admin.service'
 
 @Module({
   imports: [InfrastructureModule],
   controllers: [VendeloController, VendeloWebhookController],
+  providers: [ShippingAdminService],
 })
 export class VendeloModule {}
